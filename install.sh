97e02a487ea5c734565269f194c236b2386fdef6
Raka Teja <maderakateja@gmail.com>
2018-05-09 18:00:54 +0000

    install vim scala by wget

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Cloning molokai color ..."
 wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -O ~/.vim/colors/molokai.vim
 
+echo "Cloning scala vim ..."
+mkdir -p ~/.vim/ftdetect ~/.vim/indent ~/.vim/syntax
+for d in ftdetect indent syntax ; do wget -O ~/.vim/$d/scala.vim https://raw.githubusercontent.com/derekwyatt/vim-scala/master/$d/scala.vim; done
+
 echo "Updating vimrc ..."
 cp .vimrc ~/.vimrc
 
